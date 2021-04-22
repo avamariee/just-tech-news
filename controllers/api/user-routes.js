d2e489a4bb5026db856572bc97f9441c36a2ef6c
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-21 20:32:20 -0600

    added editing and deleting posts with authorization

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require("../../models");
+const withAuth = require('../../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -58,7 +59,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -107,7 +108,7 @@ router.post('/login', (req, res) => {
 
 
 
-  router.post('/logout', (req, res) => {
+  router.post('/logout', withAuth, (req, res) => {
 
     if (req.session.loggedIn) {
       req.session.destroy(() => {
@@ -123,7 +124,7 @@ router.post('/login', (req, res) => {
   });
 
 // PUT /api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
         individualHooks: true,
@@ -145,7 +146,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where:{
             id: req.params.id
